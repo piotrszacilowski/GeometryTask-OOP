2c070ccae1d9bbfbb891919ddb15fd033f3e4eab
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-03-12 03:07:58 +0100

    Implementatnion of methods getArea and getCircuit to Rhombus class

--- src/main/Rhombus.java
+++ src/main/Rhombus.java
@@ -1,4 +1,21 @@
 package main;
 
-public class Rhombus {
+public class Rhombus implements Figure {
+    private final double side;
+    private final double height;
+
+    public Rhombus(double side, double height) {
+        this.side = side;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return side * height;
+    }
+
+    @Override
+    public double getCircuit() {
+        return 4 * side;
+    }
 }
