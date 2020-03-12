bd8d7a0b128632286ebe68f3de5f3e0a3e9547a4
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-03-12 03:05:00 +0100

    Added implementation methods from Figure interface to Triangle class

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -1,4 +1,25 @@
 package main;
 
-public class Triangle {
+public class Triangle implements Figure {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
+    private final double height;
+
+    public Triangle(double sideA, double sideB, double sideC, double height) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * sideA * height;
+    }
+
+    @Override
+    public double getCircuit() {
+        return sideA + sideB + sideC;
+    }
 }
