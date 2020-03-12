f870f20a3eb1c62c372b484bac1d856b654d8d00
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-03-12 02:52:19 +0100

    Implementatnion of methods getArea and getCircuit to Rectangle class

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -1,5 +1,21 @@
 package main;
 
-public class Rectangle {
+public class Rectangle implements Figure {
+    private final double sideA;
+    private final double sideB;
 
+    public Rectangle(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double getArea() {
+        return sideA * sideB;
+    }
+
+    @Override
+    public double getCircuit() {
+        return 2 * (sideA + sideB);
+    }
 }
